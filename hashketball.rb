1c0450c4995e934085db3ee6cfae53e0af0da66a
Joseph Trettevik <josephtrettevik@gmail.com>
2019-08-06 21:55:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash ()
   hash = {
     :home => {team_name: "Brooklyn Nets", 
               colors: ["Black", "White"], 
-              players: [{:players"Alan Anderson"
+              players: [{:player_name => "Alan Anderson",
                         :number => 0,
                         :shoe => 16,
                         :points => 22,
@@ -13,8 +13,8 @@ def game_hash ()
                         :assists => 12,
                         :steals => 3,
                         :blocks => 1,
-                        :slam_dunks => 1}},
-                        {"Reggie Evans" => {
+                        :slam_dunks => 1},
+                        {:player_name => "Reggie Evans",
                         :number => 30,
                         :shoe => 14,
                         :points => 12,
@@ -22,8 +22,8 @@ def game_hash ()
                         :assists => 12,
                         :steals => 12,
                         :blocks => 12,
-                        :slam_dunks => 7}},
-                        {"Brook Lopez" => {
+                        :slam_dunks => 7},
+                        {:player_name => "Brook Lopez",
                         :number => 11,
                         :shoe => 17,
                         :points => 17,
@@ -31,7 +31,7 @@ def game_hash ()
                         :assists => 10,
                         :steals => 3,
                         :blocks => 1,
-                        :slam_dunks => 15}},
+                        :slam_dunks => 15},
                         {"Mason Plumlee" => {
                         :number => 1,
                         :shoe => 19,
