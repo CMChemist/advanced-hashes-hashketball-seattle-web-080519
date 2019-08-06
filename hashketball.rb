36baa83eed3f77b34e031cf6346c89ebb8ceefbb
Joseph Trettevik <josephtrettevik@gmail.com>
2019-08-06 21:56:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -32,7 +32,7 @@ def game_hash ()
                         :steals => 3,
                         :blocks => 1,
                         :slam_dunks => 15},
-                        {"Mason Plumlee" => {
+                        {:player_name => "Mason Plumlee",
                         :number => 1,
                         :shoe => 19,
                         :points => 26,
@@ -40,8 +40,8 @@ def game_hash ()
                         :assists => 6,
                         :steals => 3,
                         :blocks => 8,
-                        :slam_dunks => 5}},
-                        {"Jason Terry" => {
+                        :slam_dunks => 5},
+                        {:player_name => "Jason Terry",
                         :number => 31,
                         :shoe => 15,
                         :points => 19,
@@ -49,7 +49,7 @@ def game_hash ()
                         :assists => 2,
                         :steals => 4,
                         :blocks => 11,
-                        :slam_dunks => 1}}]},
+                        :slam_dunks => 1}]},
     :away => {team_name: "Charlotte Hornets", 
               colors: ["Turquoise", "Purple"], 
               players: [{"Jeff Adrien" => {
